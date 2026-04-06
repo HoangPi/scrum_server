d7563201303cf0b9fdc701926fe41189a0c9d58d
= <=>
2026-04-06 16:52:32 +0700

    fix the query

--- sql/011_project_overview.sql
+++ sql/011_project_overview.sql
@@ -9,7 +9,7 @@ CREATE OR REPLACE FUNCTION public.get_project_overview(p_pid INT)
     failed_user_stories INT
  )
 AS $$
-    SELECT COUNT(pb.id), 
+    SELECT COUNT(DISTINCT pb.id) as total, 
         SUM(CASE WHEN pb.status = 'finished'::progress_type THEN 1 ELSE 0 END),
         SUM(pb.story_point), 
         SUM(CASE WHEN pb.status = 'finished'::progress_type THEN pb.story_point ELSE 0 END) ,
@@ -17,8 +17,8 @@ AS $$
         SUM(CASE WHEN sb.status = 'finished'::progress_type THEN 1 ELSE 0 END), 
         SUM(CASE WHEN sb.status = 'failed'::progress_type THEN 1 ELSE 0 END)
     FROM ProductBacklog pb
-    INNER JOIN SprintBacklog sb ON pb.id = sb.backlog_item_id
-    WHERE pb.project_id = p_pid
+    LEFT JOIN SprintBacklog sb ON pb.id = sb.backlog_item_id
+    WHERE pb.project_id = 2
     GROUP BY pb.project_id;
 $$ LANGUAGE sql
 ;
