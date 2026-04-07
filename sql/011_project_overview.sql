f06bc4063a145f4c7dfe1d09a5fcc896ff5eab01
= <=>
2026-04-07 18:59:08 +0700

    fix overview query

--- sql/011_project_overview.sql
+++ sql/011_project_overview.sql
@@ -1,24 +1,25 @@
-CREATE OR REPLACE FUNCTION public.get_project_overview(p_pid INT)
- RETURNS TABLE (
-    total_product_backlogs INT,
-    finished_product_backlogs INT,
-    total_story_points INT,
-    finished_story_points INT,
-    total_user_stories INT,
-    finished_user_stories INT,
-    failed_user_stories INT
- )
-AS $$
-    SELECT COUNT(DISTINCT pb.id) as total, 
-        SUM(CASE WHEN pb.status = 'finished'::progress_type THEN 1 ELSE 0 END),
-        SUM(pb.story_point), 
-        SUM(CASE WHEN pb.status = 'finished'::progress_type THEN pb.story_point ELSE 0 END) ,
-        COUNT(sb.id), 
-        SUM(CASE WHEN sb.status = 'finished'::progress_type THEN 1 ELSE 0 END), 
-        SUM(CASE WHEN sb.status = 'failed'::progress_type THEN 1 ELSE 0 END)
-    FROM ProductBacklog pb
-    LEFT JOIN SprintBacklog sb ON pb.id = sb.backlog_item_id
-    WHERE pb.project_id = 2
-    GROUP BY pb.project_id;
-$$ LANGUAGE sql
+CREATE OR REPLACE FUNCTION public.get_project_overview(p_pid integer)
+ RETURNS TABLE(total_product_backlogs integer, finished_product_backlogs integer, total_story_points integer, finished_story_points integer, total_user_stories integer, finished_user_stories integer, failed_user_stories integer)
+ LANGUAGE sql
+AS $function$
+    SELECT 
+	    COUNT(DISTINCT pb.id) AS total,
+	    SUM(CASE WHEN pb.status = 'finished'::progress_type THEN 1 ELSE 0 END) AS finished_pb,
+	    SUM(pb.story_point) AS total_story_points,
+	    SUM(CASE WHEN pb.status = 'finished'::progress_type THEN pb.story_point ELSE 0 END) AS finished_story_points,
+	    SUM(sb_stats.total_sb),
+	    SUM(sb_stats.finished_sb),
+	    SUM(sb_stats.failed_sb)
+	FROM ProductBacklog pb
+	LEFT JOIN (
+	    SELECT backlog_item_id,
+	           COUNT(DISTINCT id) AS total_sb,
+	           SUM(CASE WHEN status = 'finished'::progress_type THEN 1 ELSE 0 END) AS finished_sb,
+	           SUM(CASE WHEN status = 'failed'::progress_type THEN 1 ELSE 0 END) AS failed_sb
+	    FROM SprintBacklog
+	    GROUP BY backlog_item_id
+	) sb_stats ON pb.id = sb_stats.backlog_item_id
+	WHERE pb.project_id = p_pid
+	GROUP BY pb.project_id;
+$function$
 ;
